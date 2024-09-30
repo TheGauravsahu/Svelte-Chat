e98fa571ae87e412416f7dc90f3366c6ed60d048
TheGauravsahu <itsgauravsahu7@gmail.com>
2024-09-30 15:58:11 +0530

    Fixed Display Name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,11 +16,17 @@
 	<nav class="flex items-center justify-around border-b p-4 mb-4 px-8">
 		<h1 class="text-2xl">ChatApp</h1>
 		<div class="flex items-center gap-4">
-			<p>Welcome {$user.displayName}</p>
+			{#if $user.displayName}
+				<p>Welcome {$user.displayName}</p>
+			{/if}
 			{#if $user.photoURL}
 				<img class="rounded-full w-8" src={$user.photoURL} alt="" />
 			{:else}
-				<img class="rounded-full w-8" src="https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg" alt="" />
+				<img
+					class="rounded-full w-8"
+					src="https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg"
+					alt=""
+				/>
 			{/if}
 			<button on:click={() => signOut(auth)} class="btn">Logout</button>
 		</div>
